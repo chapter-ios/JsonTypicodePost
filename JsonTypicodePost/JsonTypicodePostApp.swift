0ae2065f6085c4be8db37add1de3cdec8152ce23
Satori <satriya.wicaksana@phincon.com>
2025-11-21 09:58:25 +0700

    Initial Commit

---
+++ JsonTypicodePost/JsonTypicodePostApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonTypicodePostApp.swift
+//  JsonTypicodePost
+//
+//  Created by Satori on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct JsonTypicodePostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
